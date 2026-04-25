b5d45c79cb2f383b6da2c846189408dee82f3d86
John Pardo <johnlenonpardo@gmail.com>
2026-04-24 22:16:45 -0300

    Agregar Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,3 +52,4 @@ export default function RootLayout({
     </html>
   );
 }
+
